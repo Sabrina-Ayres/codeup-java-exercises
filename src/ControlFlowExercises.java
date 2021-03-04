b8a5ff8583cf740ad61d403323255f0a4b34c970
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2021-03-04 15:31:54 -0600

    updating exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,12 +3,41 @@ public class ControlFlowExercises {
 
     public static void main(String[] args) {
 
-        int i = 5;
+//        int i = 5;
+//
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
 
-        while (i <= 15) {
-            System.out.println(i);
-            i++;
-        }
+//        int i = 100;
+//        do {
+//            i = i - 5;
+//            System.out.println(i);
+//        }
+//        while (i > -10);
+
+
+//        long i = 2;
+//        do {
+//            i = i * i;
+//            System.out.println(i);
+//        }
+//        while (i < 1000000);
+
+//        for(int i = 1; i <= 100; i++) {
+//            if (i % 3 == 1) {
+//                System.out.println("Fizz");
+//            }
+//            if (i % 5 == 1) {
+//                System.out.println("Buzz");
+//            }
+//            if (i % 3 == 1 && i % 5 == 1) {
+//                System.out.println("FizzBuzz");
+//            }
+//        }
+
+
+
 
     }
 }
