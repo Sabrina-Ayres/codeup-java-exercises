fa638a3f15a981d80f45dff69b473b3bdee0a415
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2021-03-04 16:58:58 -0600

    Updating

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ControlFlowExercises {
 
@@ -9,12 +11,15 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //            i++;
 
+
+
 //        int i = 100;
 //        do {
 //            i = i - 5;
 //            System.out.println(i);
 //        }
 //        while (i > -10);
+
 
 
 //        long i = 2;
@@ -37,6 +42,50 @@ public class ControlFlowExercises {
 //        }
 
 
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("Enter a number:");
+//        int userNum = sc.nextInt();
+//        long i = 1;
+//
+//        System.out.println("You entered number " + userNum + "!");
+//        System.out.println();
+//        System.out.println("Here is your table!");
+//        System.out.println();
+//        System.out.println("number | squared | cubed");
+//        System.out.println("------ | ------- | -----");
+//        while (i <= userNum) {
+//            long numSq = i * i;
+//            long numCub = i * i * i;
+//            int n = 5;
+//            String s = String.format("%1$"+n+"s", "");
+//            System.out.println(i + s + " | " + numSq + s + "  | " + numCub);
+//            i++;
+//        }
+
+
+
+        Scanner sc = new Scanner(System.in);
+
+//        System.out.print("Do you want to enter a grade? ");
+//        String userResponse = sc.nextLine().toLowerCase();
+//
+//        if (userResponse == "Yes")
+        System.out.print("Enter your grade: ");
+        int userNum = sc.nextInt();
+
+        if (userNum <= 100 && userNum >= 88) {
+            System.out.print("You got an A!");
+        } else if (userNum < 88 && userNum >= 80) {
+            System.out.print("You got an B!");
+        } else if (userNum < 80 && userNum >= 67) {
+            System.out.print("You got an C!");
+        } else if (userNum < 67 && userNum >= 60) {
+            System.out.print("You got an D!");
+        } else if (userNum <= 59) {
+            System.out.print("Bummer! You got an F!");
+        }
 
 
     }
